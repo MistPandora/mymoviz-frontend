cc0e7425958943702e88ffc2e84ebae190cf77d3
Killyan <killyan.faucon@hotmail.fr>
2023-11-15 12:20:17 +0100

    adding fetch url

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch('https://mymoviz-backend-coral-theta.vercel.app/movies').then(r => r.json()).then(data => { setMoviesData(data.results) })
+    fetch('https://mymoviz-backend-navy.vercel.app/movies').then(r => r.json()).then(data => { setMoviesData(data.results) })
   }, [])
 
 
